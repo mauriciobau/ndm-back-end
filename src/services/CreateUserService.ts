fb8f4af946b81f9fb9837093bbfa504c73830586
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-05-09 17:27:50 -0300

    Criptografia de senha

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,6 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +21,12 @@ class CreateUserService {
       throw new Error('Email address already used.');
     }
 
+    const hashdPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashdPassword,
     });
 
     await usersRepository.save(user);
