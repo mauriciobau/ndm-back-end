41431ff6ca613d2058f3885353a5fe573346436e
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-05-24 15:38:39 -0300

    Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
-import appointmentsRouter from '../infra/http/routes/appointments.routes';
 
 interface IRequest {
   provider_id: string;
@@ -34,9 +34,25 @@ class ListProvidersMonthAvailabilityService {
       },
     );
 
-    console.log(appointments);
+    const numbersOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 1, available: false }];
+    const eachDayArray = Array.from(
+      { length: numbersOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
