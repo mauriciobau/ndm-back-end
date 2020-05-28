9ab085ac9e5656e8723187577ea1a6c9209d76b9
Maurício A. Baú <mauricio.a.bau@gmail.com>
2020-05-27 23:07:21 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'email_cadastrado_no_ses@dominio.com',
+      name: 'Admin Gobarber',
+    },
+  },
+} as IMailConfig;
